170cb8dfe3f885f9910b1ba91a7d389ed74c44ca
zOmArRD <omarsena25@gmail.com>
2020-09-04 21:44:54 +0200

    Small changes

--- src/system/Main.php
+++ src/system/Main.php
@@ -15,7 +15,6 @@ declare(strict_types=1);
 namespace system;
 
 /** Pocketmine */
-
 use pocketmine\entity\Entity;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\plugin\PluginBase;
